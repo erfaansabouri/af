67b670234e045d6e388f73de7681b5595b876fad
Erfan Sabouri <erfaansabouri@gmail.com>
2024-05-02 18:56:54 +0330

    fix

--- app/Http/Controllers/Admin/ExportController.php
+++ app/Http/Controllers/Admin/ExportController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Exports\DebtExport;
+use App\Exports\TransactionExport;
 use App\Http\Controllers\Controller;
 use App\Models\Tenant;
 use Illuminate\Http\Request;
+use Maatwebsite\Excel\Facades\Excel;
 
 class ExportController extends Controller {
     public function index ( Request $request ) {
-
+        return Excel::download(new DebtExport() , 'debt.xlsx');
     }
 }
