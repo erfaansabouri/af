c2cef928ca037c73b29f681deef4b308e1016125
Erfan Sabouri <erfaansabouri@gmail.com>
2024-07-21 08:58:57 +0330

    fix

--- app/Console/Commands/WarningCommand.php
+++ app/Console/Commands/WarningCommand.php
@@ -26,7 +26,7 @@ class WarningCommand extends Command {
      */
     public function handle () {
         $monthly_charges = MonthlyCharge::query()
-                                        ->where('due_date' , '<' , now()->subDays(30))
+                                        ->where('due_date' , '<' , now()->subDays(31))
                                         ->whereNull('paid_at')
                                         ->get();
         foreach ( $monthly_charges as $monthly_charge ) {
