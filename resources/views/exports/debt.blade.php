76eda073787d2551cb79acd08415f1b2325f9638
Erfan Sabouri <erfaansabouri@gmail.com>
2024-07-31 20:57:21 +0330

    fix

--- resources/views/exports/debt.blade.php
+++ resources/views/exports/debt.blade.php
@@ -5,6 +5,7 @@
         <th>تعداد اخطار</th>
         <th>مبلغ شارژ عقب افتاده</th>
         <th>مبلغ بدهی</th>
+        <th>مبلغ شارژ ثابت ماهیانه</th>
     </tr>
     </thead>
     <tbody>
@@ -14,6 +15,7 @@
             <td>{{ $tenant->warnings()->count() }}</td>
             <td>{{ $tenant->passed_due_date_amount }}</td>
             <td>{{ $tenant->debts()->notPaid()->sum('amount') }}</td>
+            <td>{{ $tenant->monthly_charge_amount }}</td>
         </tr>
     @endforeach
     </tbody>
