c0fc48c5ec5e952841089448150b19244a9ba148
Erfan Sabouri <erfaansabouri@gmail.com>
2024-09-09 19:42:41 +0330

    style

--- resources/views/metronic/admin/transactions/index.blade.php
+++ resources/views/metronic/admin/transactions/index.blade.php
@@ -138,8 +138,7 @@
                             <caption>{{$model_name}} ها</caption>
                             <thead class="thead-light iransans-web">
                             <tr>
-                                <th class="iransans-web">شناسه</th>
-                                <th class="iransans-web">شماره تراکنش</th>
+                                <th class="iransans-web">آی دی سیستم</th>
                                 <th class="iransans-web">پلاک</th>
                                 <th class="iransans-web">موضوع پرداخت</th>
                                 <th class="iransans-web">مبلغ</th>
@@ -153,7 +152,6 @@
                             @foreach($records as $record)
                                 <tr>
                                     <td class="iransans-web">{{ $record->id }}</td>
-                                    <td class="iransans-web">{{ $record->tx_id }}</td>
                                     <td class="iransans-web">{{ $record->tenant->plaque }}</td>
                                     <td class="iransans-web">{{ $record->subject }}</td>
                                     <td class="iransans-web">{{ number_format($record->amount) }} ریال</td>
@@ -187,7 +185,6 @@
                                                             <br><b>{{ $v }}</b>
                                                             <hr>
                                                         @endif
-
                                                     @endforeach
                                                 @endif
                                                 @if($verify_log->exception_message)
