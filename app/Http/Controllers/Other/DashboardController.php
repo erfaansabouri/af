aa57fe9ee7188d18ef913f6576249a5006c60ced
Erfan Sabouri <erfaansabouri@gmail.com>
2025-06-25 23:28:29 +0330

    fix: 🐛 add other log

--- app/Http/Controllers/Other/DashboardController.php
+++ app/Http/Controllers/Other/DashboardController.php
@@ -4,12 +4,18 @@ namespace App\Http\Controllers\Other;
 
 use App\Http\Controllers\Controller;
 use App\Models\Message;
+use App\Models\OtherFinancialPeriodLog;
 use App\Models\Warning;
 use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
     public function dashboard(){
-        return view('metronic.other.dashboard.dashboard');
+        $other = \auth('other')->user();
+        $ended_financials = OtherFinancialPeriodLog::query()
+            ->where('ended_at', '<', now())
+            ->where('other_id', $other->id)
+            ->get();
+        return view('metronic.other.dashboard.dashboard', compact('ended_financials'));
     }
 }
